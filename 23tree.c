7f03f49e317dd8d42be511af79f8a21635cfc349
blancaveber <dimetra.09@mail.ru>
2017-05-03 07:52:56 +0300

    Хотим, чтобы нода `child` являлась потомком ноды `parent`; 4 функции для вращения поддерева

--- 23tree.c
+++ 23tree.c
@@ -59,3 +59,63 @@ int get_balance_factor(Node* node) {
     }
     return bf;
 }
+void set_parentness(Node* parent, Node* child, int is_left) {
+    if (is_left) {
+        parent->left = child;
+    } else {
+        parent->right = child;
+    }
+    if (child != NULL) {
+        child->parent = parent;
+    }
+}
+
+
+Node* rotate_small_right(Node* node) {
+    Node* a = node;
+    Node* b = a->left;
+    set_parentness(a, b->right, 1);
+    Node* a_parent = a->parent;
+    set_parentness(b, a, 0);
+    b->parent = a_parent;
+    return b;
+}
+
+
+Node* rotate_big_right(Node* node) {
+    Node* a = node;
+    Node* b = a->left;
+    Node* c = b->right;
+    set_parentness(a, c->right, 1);
+    set_parentness(b, c->left, 0);
+    set_parentness(c, b, 1);
+    Node* a_parent = a->parent;
+    set_parentness(c, a, 0);
+    c->parent = a_parent;
+    return c;
+}
+
+
+Node* rotate_big_left(Node* node) {
+    Node* a = node;
+    Node* b = a->right;
+    Node* c = b->left;
+    set_parentness(a, c->left, 0);
+    set_parentness(b, c->right, 1);
+    set_parentness(c, b, 0);
+    Node* a_parent = a->parent;
+    set_parentness(c, a, 1);
+    c->parent = a_parent;
+    return c;
+}
+
+
+Node* rotate_small_left(Node* node) {
+    Node* a = node;
+    Node* b = a->right;
+    set_parentness(a, b->left, 0);
+    Node* a_parent = a->parent;
+    set_parentness(b, a, 1);
+    b->parent = a_parent;
+    return b;
+}
